a9bb272595131ebdaca3a5c9ae097783dc472cb4
Mucunguzi <moemucu@gmail.com>
2026-04-07 21:35:10 +0300

    Use relative paths for Pages output

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://mucunguzi.tech",
-  base: "/",
+  base: "./",
   output: "static",
   build: {
     assets: "assets",
